e956e094dc48ac66ccf74dec594c934134a67ee4
Vasil Yoshev <vasily@axilion.com>
2022-10-09 15:30:06 +0300

    Show avatar and username for logged users

--- src/components/TopAppBar/TopAppBar.tsx
+++ src/components/TopAppBar/TopAppBar.tsx
@@ -1,34 +1,44 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import { LoginDialog } from 'components';
-import { selectIsLoggedIn } from 'slices';
+import { selectIsLoggedIn, selectUsername } from 'slices';
 import styles from './TopAppBar.module.scss';
 
 export const TopAppBar = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const username = useSelector(selectUsername);
   const [isLoginDialogOpen, setIsLoginDialogOpen] = useState(false);
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography
-          variant="h6"
-          component="div"
-          classes={{ root: styles.welcomeTypographyRoot }}
-        >
-          Welcome
-        </Typography>
-        {!isLoggedIn && (
-          <Button color="inherit" onClick={() => setIsLoginDialogOpen(true)}>
-            Login
-          </Button>
-        )}
-        <LoginDialog
-          isOpen={isLoginDialogOpen}
-          close={() => setIsLoginDialogOpen(false)}
-        />
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            classes={{ root: styles.welcomeTypographyRoot }}
+          >
+            Welcome
+          </Typography>
+          {!isLoggedIn && (
+            <Button color="inherit" onClick={() => setIsLoginDialogOpen(true)}>
+              Login
+            </Button>
+          )}
+          {isLoggedIn && (
+            <div className={styles.userInfoWrapper}>
+              <span>{username}</span>
+              <AccountCircle />
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      <LoginDialog
+        isOpen={isLoginDialogOpen}
+        close={() => setIsLoginDialogOpen(false)}
+      />
+    </>
   );
 };
